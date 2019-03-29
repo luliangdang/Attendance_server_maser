0906dee5823384e8b55bdbbb483000b32fef91d7
luliangdang <806034232@qq.com>
2019-03-30 03:28:30 +0800

    Create the project

---
+++ Attendance_server_master/log.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Attendance_server_master
+{
+    partial class log
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbPasswd = new System.Windows.Forms.TextBox();
+            this.btLog = new System.Windows.Forms.Button();
+            this.btReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "用户名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "密码：";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(102, 54);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(132, 21);
+            this.tbName.TabIndex = 2;
+            // 
+            // tbPasswd
+            // 
+            this.tbPasswd.Location = new System.Drawing.Point(102, 107);
+            this.tbPasswd.Name = "tbPasswd";
+            this.tbPasswd.PasswordChar = '*';
+            this.tbPasswd.Size = new System.Drawing.Size(132, 21);
+            this.tbPasswd.TabIndex = 3;
+            this.tbPasswd.UseSystemPasswordChar = true;
+            this.tbPasswd.WordWrap = false;
+            // 
+            // btLog
+            // 
+            this.btLog.Location = new System.Drawing.Point(70, 150);
+            this.btLog.Name = "btLog";
+            this.btLog.Size = new System.Drawing.Size(75, 23);
+            this.btLog.TabIndex = 4;
+            this.btLog.Text = "登录";
+            this.btLog.UseVisualStyleBackColor = true;
+            this.btLog.Click += new System.EventHandler(this.btLog_Click);
+            // 
+            // btReset
+            // 
+            this.btReset.Location = new System.Drawing.Point(169, 150);
+            this.btReset.Name = "btReset";
+            this.btReset.Size = new System.Drawing.Size(75, 23);
+            this.btReset.TabIndex = 5;
+            this.btReset.Text = "忘记密码";
+            this.btReset.UseVisualStyleBackColor = true;
+            // 
+            // log
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 215);
+            this.Controls.Add(this.btReset);
+            this.Controls.Add(this.btLog);
+            this.Controls.Add(this.tbPasswd);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "log";
+            this.Text = "智能考勤系统";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbPasswd;
+        private System.Windows.Forms.Button btLog;
+        private System.Windows.Forms.Button btReset;
+    }
+}
+
---
+++ Attendance_server_master/log.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Attendance_server_master
+{
+    public partial class log : Form
+    {
+        public log()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 点击登录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btLog_Click(object sender, EventArgs e)
+        {
+            if(tbName.Text=="")
+            {
+                MessageBox.Show("用户名不得为空!");
+            }
+            else if(tbPasswd.Text == "")
+            {
+                MessageBox.Show("密码不得为空!");
+            }
+            else if(tbName.Text !=""&&tbPasswd.Text != "")
+            {
+                MessageBox.Show("开始登录！");
+            }
+        }
+    }
+}
